7ccd07beb223cbf3726acb2e526784a8b6c43b10
Thaksinarat <Thaksinarat.s@gmail.com>
2025-11-07 20:11:17 +0700

    New Artwork.jsx

---
+++ src/components/section/Artwork.jsx
@@ -0,0 +1,12 @@
+import { RevealOnScroll } from "../RevealOnScroll"
+
+export const Artwork = () => {
+    return <>
+        <section id="Artwork" className="min-h-screen flex items-center justify-center py-20">
+            <RevealOnScroll>
+                
+            </RevealOnScroll>
+        </section>
+    </>
+
+}
